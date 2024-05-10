9f5fb032960c21e8e8c64e0cb95dd6ca8a0fcf5b
umeneses <umeneses@student.42.fr>
2024-05-10 12:20:16 -0300

    improving automated test

--- _ci_tdd/test_files/test_main.c
+++ _ci_tdd/test_files/test_main.c
@@ -6,24 +6,28 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 19:06:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/09 19:07:18 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/10 11:41:22 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <asset.h>
+#include <assert.h>
 #include "../src/main.c"
+#include "../headers/push_swap.h"
 
-void test_add() {
+void test_add()
+{
 	assert(add(5, 3) == 8);
-	printf("add function test passed\n");
+	ft_printf("add function test passed\n");
 }
 
-void test_subtract() {
+void test_subtract()
+{
 	assert(subtract(5, 3) == 2);
-	printf("subtract function test passed\n");
+	ft_printf("subtract function test passed\n");
 }
 
-int main() {
+int main()
+{
 	test_add();
 	test_subtract();
 	return 0;
