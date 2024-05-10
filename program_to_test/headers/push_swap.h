015894e6745d49ff0506501d79cac6536a7a41f4
umeneses <umeneses@student.42.fr>
2024-05-10 18:08:14 -0300

    moving all push_swap files to a new folder

--- program_to_test/headers/push_swap.h
+++ program_to_test/headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 15:23:33 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/09 18:36:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/10 14:54:28 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 # define PUSH_SWAP_H
 
 # include "../libs/libft/includes/libft.h"
+
+int	push_add(int a, int b);
+int push_subtract(int a, int b);
 
 #endif
--- program_to_test/src/push_main.c
+++ program_to_test/src/push_main.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_main.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/10 11:27:06 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/10 17:57:05 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	add(int a, int b)
+int	push_add(int a, int b)
 {
 	return (a + b);
 }
 
-int subtract(int a, int b)
+int push_subtract(int a, int b)
 {
 	return (a - b);
 }
@@ -26,12 +26,12 @@ int main(void)
 {
 	ft_printf("\033[0;35m");
 	ft_printf("this is the push swap main running\n");
-	int result = add(5, 3);
-	ft_printf("Operation: add(5, 3)\n");
+	int result = push_add(5, 3);
+	ft_printf("Operation: psuh_add(5, 3)\n");
 	ft_printf("The sum is: %d\n", result);
 	ft_printf("///////////////\n");
-	result = subtract(5, 3);
-	ft_printf("Operation: subtract(5, 3)\n");
+	result = push_subtract(5, 3);
+	ft_printf("Operation: push_subtract(5, 3)\n");
     ft_printf("The difference is: %d\n", result);
 	return (0);
 }
