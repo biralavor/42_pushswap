fab056b041ed75e07aafc284b68cd6c9350d1e39
umeneses <umeneses@student.42sp.org.br>
2024-05-13 16:12:58 -0300

    building a tester with CUnit framework

--- _ci_tdd/test_files/test_main_miunit.c
+++ _ci_tdd/test_files/test_main_miunit.c
@@ -1,27 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_main.c                                        :+:      :+:    :+:   */
+/*   test_main_miunit.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 19:06:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/13 12:45:20 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/13 16:10:15 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TESTING
-
-# include "minunit.h"
-# include "push_swap.h"
-
-# include "../../program_to_test/src/push_main.c"
+#include "minunit.h"
+#include "push_swap.h"
+#include "../../program_to_test/src/push_functions.c"
 
 MU_TEST(test_pushadd_5plus4)
 {
 	// ARRANGE
 	int	actual_result;
-	int	expected_result = 10;
+	int	expected_result = 9;
 
 	// ACT
 	actual_result = push_add(5, 4);
@@ -30,7 +27,7 @@ MU_TEST(test_pushadd_5plus4)
 	mu_assert_int_eq(expected_result, actual_result);
 }
 
-MU_TEST(test_pushsubtract_5minus3)
+void test_pushsubtract_5minus3()
 {
 	// ARRANGE
 	int	actual_result;
@@ -49,11 +46,9 @@ MU_TEST_SUITE(first_tests)
 	MU_RUN_TEST(test_pushsubtract_5minus3);
 }
 
-int	main(void)
+int main(void)
 {
 	MU_RUN_SUITE(first_tests);
 	MU_REPORT();
-	return (MU_EXIT_CODE);
+	return (0);
 }
-
-#endif
