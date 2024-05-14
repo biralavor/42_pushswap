20b80d4b40d7c6ae2a04140fb5a5b789e9f28ce1
umeneses <umeneses@student.42sp.org.br>
2024-05-14 11:31:17 -0300

    prototyping initial functions

--- program_to_test/headers/push_swap.h
+++ program_to_test/headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 15:23:33 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/13 14:49:27 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/14 11:36:25 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,40 @@
 # define PUSH_SWAP_H
 
 # include "../libs/libft/includes/libft.h"
+# include <stdbool.h>
+
+typedef struct s_stack_a
+{
+	int				nbr;
+	struct stack_a	*prev;
+	struct stack_a	*next;
+}					t_stack_a;
+
+typedef struct s_stack_b
+{
+	int				nbr;
+	struct stack_b	*prev;
+	struct stack_b	*next;
+}					t_stack_b;
+
+void	swap_a(t_stack_a **stack_a);
+void	swap_b(t_stack_a **stack_b);
+void	swap_ab(t_stack_a **stack_a, t_stack_b **stack_b);
+void	push_a(t_stack_a **stack_a, t_stack_b **stack_b);
+void	push_b(t_stack_a **stack_a, t_stack_b **stack_b);
+void	rotate_a(t_stack_a **stack_a);
+void	rotate_b(t_stack_b **stack_b);
+void	double_rotate_ab(t_stack_a **stack_a, t_stack_b **stack_b);
+void	reverse_rotate_a(t_stack_a **stack_a);
+void	reverse_rotate_b(t_stack_b **stack_b);
+void	double_reverse_rotate_ab(t_stack_a **stack_a, t_stack_b **stack_b);
+/* initial push_swap functions */
+
+bool	is_sorted(t_stack_a *stack_a);
+/* validation functions */
 
 int	push_add(int a, int b);
 int push_subtract(int a, int b);
+/* tester functions */
 
 #endif
