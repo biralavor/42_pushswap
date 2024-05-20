2c9deee84bcbdc231e669fcd36561beeaccf3f0c
umeneses <umeneses@student.42sp.org.br>
2024-05-20 18:00:53 -0300

    testing new version of ft_lstend_int

--- program_to_test/src/push_main.c
+++ program_to_test/src/push_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/20 12:37:47 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/20 15:54:57 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,30 @@
 
 int main(void)
 {
-	int		result;
-	int		add_one;
-	int		add_two;
-	int		*add_one_ptr;
-	int		*add_two_ptr;
-	int		*toprint = NULL;
-	t_list	*new_node;
+	int top = 11;
+	int second = 22;
+	int	third = 33;
+	int	fourth = 44;
+	int bottom = 99;
+	t_stack	*stack;
+	t_stack	*temp;
 
-	add_one = 42;
-	add_two = 21;
-	add_one_ptr = &add_one;
-	add_two_ptr = &add_two;
-	new_node = (t_list *)ft_calloc(1, sizeof(t_list));
-	if (new_node == NULL)
+	stack = (t_stack *)ft_calloc(1, sizeof(t_stack));
+	if (stack == NULL)
 		ft_error_msg("Memory allocation failed\n");
-	new_node->content = add_one_ptr;
-	new_node->next = NULL;
-	new_node = ft_lstend_void(new_node, add_two_ptr);
-	t_list	*temp_node;
-	temp_node = new_node;
-	while (temp_node != NULL)
+	stack->next = NULL;
+	stack = ft_lstend_int(stack, top);
+	stack = ft_lstend_int(stack, second);
+	stack = ft_lstend_int(stack, third);
+	stack = ft_lstend_int(stack, fourth);
+	stack = ft_lstend_int(stack, bottom);
+	temp = stack;
+	ft_printf("list size = %d\n", ft_lstsize_int((t_list *)temp));
+	ft_printf("The content is: %d\n", temp->nbr);
+	while (temp->next != NULL)
 	{
-		toprint = temp_node->content;
-		ft_printf("The content is: %d\n", *toprint);
-		temp_node = temp_node->next;
+		temp = temp->next;
+		ft_printf("The content is: %d\n", temp->nbr);
 	}
-	ft_printf("///////////////\n");
-	ft_printf("\033[0;35m");
-	ft_printf("this is the push swap main running\n");
-	result = push_add(5, 3);
-	ft_printf("Operation: psuh_add(5, 3)\n");
-	ft_printf("The sum is: %d\n", result);
-	ft_printf("///////////////\n");
-	result = push_subtract(5, 3);
-	ft_printf("Operation: push_subtract(5, 3)\n");
-    ft_printf("The difference is: %d\n", result);
 	return (0);
 }
