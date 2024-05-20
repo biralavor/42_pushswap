3bf6ef6bc2850d0577e3829a94688d57e2ce85df
umeneses <umeneses@student.42sp.org.br>
2024-05-20 17:57:03 -0300

    adding ft_lst_init (fully), ft_push_a and ft_push_b (prototype only)

--- program_to_test/headers/push_swap.h
+++ program_to_test/headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 15:23:33 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/20 12:52:38 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/20 15:15:05 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ typedef struct s_stack
 }					t_stack;
 
 t_stack	*ft_lstend_int(t_stack *list, int value);
+t_stack	*ft_lst_init(int value);
 /* new functions for psuh to be added in LIBFT */
 
 void	ft_swap(t_stack **stack);
 void	ft_swap_ab(t_stack **stack_a, t_stack **stack_b);
-void	ft_push(t_stack **stack_a, t_stack **stack_b);
+void	ft_push_a(t_stack **stack_a, t_stack **stack_b);
+void	ft_push_b(t_stack **stack_a, t_stack **stack_b);
 void	ft_rotate_a(t_stack **stack);
 void	ft_double_rotate_ab(t_stack **stack_a, t_stack **stack_b);
 void	ft_reverse_rotate(t_stack **stack);
