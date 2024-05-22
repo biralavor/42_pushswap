59c1f7a86f6bead5286c38517b02fc3cec6b7e5c
umeneses <umeneses@student.42sp.org.br>
2024-05-22 14:55:00 -0300

    fixing ft_push_a, to push a COPY of the node from B to A, adding it on the top of all

--- program_to_test/src/ft_push.c
+++ program_to_test/src/ft_push.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 11:52:34 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/22 14:48:30 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:53:28 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 void	ft_push_a(t_stack **stack_a, t_stack **stack_b)
 {
 	int	tobe_pushed;
-	t_stack	*temp;
+	// t_stack	*temp;
 	
 	if(*stack_b == NULL)
 		return ;
-	tobe_pushed = (*stack_a)->nbr;
-	(*stack_a)->nbr = (*stack_b)->nbr;
-	temp = (t_stack *)ft_calloc(1, sizeof(t_stack));
-	temp->nbr = tobe_pushed;
-	temp->next = NULL;
-	*stack_a = ft_lst_addto_begin(*stack_a, temp);
+	tobe_pushed = (*stack_b)->nbr;
+	*stack_a = ft_lst_addto_begin(*stack_a, ft_lst_init(tobe_pushed));
+	// (*stack_a)->nbr = (*stack_b)->nbr;
+	// temp = (t_stack *)ft_calloc(1, sizeof(t_stack));
+	// temp->nbr = tobe_pushed;
+	// temp->next = NULL;
 	// while ((*stack_b)->next != NULL)
 	// {
 	// 	temp->nbr = (*stack_b)->nbr;
