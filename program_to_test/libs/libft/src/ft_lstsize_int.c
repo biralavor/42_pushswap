f0bf7046a19ff719eaaebdb4afcae2a389a6b53f
umeneses <umeneses@student.42sp.org.br>
2024-05-22 10:21:11 -0300

    LIBFT - protecting function if the list is NULL

--- program_to_test/libs/libft/src/ft_lstsize_int.c
+++ program_to_test/libs/libft/src/ft_lstsize_int.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 12:39:15 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/19 10:51:55 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/21 16:01:52 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_lstsize_int(t_list *lst)
 	int	size;
 
 	size = 0;
+	if (!lst)
+		return (size);
 	while (lst)
 	{
 		lst = lst-> next;
