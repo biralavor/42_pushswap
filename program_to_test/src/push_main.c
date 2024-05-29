acebe4bdc76e353325653f0d4307a07d1486d703
umeneses <umeneses@student.42sp.org.br>
2024-05-29 14:38:01 -0300

    first version of capturing arguments from user

--- program_to_test/src/push_main.c
+++ program_to_test/src/push_main.c
@@ -6,26 +6,29 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/28 18:40:34 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/05/29 14:37:05 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		top;
-	int		second;
-	int		bottom;
+	int		userinput;
+	int		index;
 	t_stack	*stack;
 
-	top = 11;
-	second = 22;
-	bottom = 99;
+	if (argc <= 2)
+	{
+		ft_error_msg("Not enough arguments. Please provide at leats 3 numbers");
+	}
 	stack = NULL;
-	stack = ft_lst_init(top);
-	stack = ft_lst_addto_end(stack, ft_lst_init(second));
-	stack = ft_lst_addto_end(stack, ft_lst_init(bottom));
+	index = 0;
+	while (argv[++index] != NULL)
+	{
+		userinput = ft_atoi(argv[index]);
+		stack = ft_lst_addto_end(stack, ft_lst_init( userinput));
+	}
 	ft_printf("list size = %d\n", ft_lstsize_int((t_list *)stack));
 	ft_lst_printf_int_content("stack is:", stack);
 	ft_lstclear_single_ptr(stack);
