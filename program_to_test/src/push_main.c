0f646192c54b713609371482f4faaed0c1819cc2
umeneses <umeneses@student.42sp.org.br>
2024-06-03 17:59:10 -0300

    updating user feedback

--- program_to_test/src/push_main.c
+++ program_to_test/src/push_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/05/29 15:28:48 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/03 17:58:18 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	main(int argc, char **argv)
 
 	if (argc <= 2)
 	{
-		ft_error_msg("Not enough arguments. Please provide at leats 3 numbers");
+		ft_error_msg("Not enough arguments. Please provide at leats 2 numbers");
 	}
 	stack = NULL;
 	stack = ft_lts_buildstack_argv(stack, argv);
-	ft_printf("list size = %d\n", ft_lstsize_int((t_list *)stack));
+	ft_printf("push main // list size = %d\n", ft_lstsize_int((t_list *)stack));
 	ft_lst_printf_int_content("stack is:", stack);
 	ft_lstclear_single_ptr(stack);
 	return (0);
