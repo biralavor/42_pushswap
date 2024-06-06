ef8fc041feb84b3f8cf3dc70d72817560cf782a0
umeneses <umeneses@student.42sp.org.br>
2024-06-06 15:30:09 -0300

    updating pointer caller

--- program_to_test/src/push_main.c
+++ program_to_test/src/push_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/05 12:07:50 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:15:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,27 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack	*list;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
 	if (argc <= 2)
 	{
 		ft_error_msg("Not enough arguments. Please provide at leats 2 numbers");
 	}
-	list = NULL;
-	list = ft_lts_buildstack_argv(list, argv);
-	ft_lst_printf_int_content("testing main list content:", list);
-	ft_printf("testing main list size: %d\n", ft_lstsize_int((t_list *)list));
-	if (!ft_is_sorted(list))
+	stack_a = NULL;
+	stack_b = NULL;
+	stack_a = ft_lts_buildstack_argv(&stack_a, argv);
+	ft_lst_printf_int_content("testing main list content:", stack_a);
+	ft_printf("testing main list size: %d\n", ft_lstsize_int((t_list *)stack_a));
+	if (!ft_is_sorted(stack_a))
 	{
-		if (ft_lstsize_int((t_list *)list) == 2)
-			ft_sort_2_nbrs(&list);
-		if (ft_lstsize_int((t_list *)list) == 3)
-			ft_sort_3_nbrs(&list);
-		ft_lst_printf_int_content("updated main list content:", list);
+		if (ft_lstsize_int((t_list *)stack_a) == 2)
+			ft_sort_2_nbrs(&stack_a);
+		if (ft_lstsize_int((t_list *)stack_a) == 3)
+			ft_sort_3_nbrs(&stack_a);
+		ft_lst_printf_int_content("updated main list content:", stack_a);
 	}
-	ft_lstclear_single_ptr(list);
+	ft_lstclear_single_ptr(stack_a);
+	ft_lstclear_single_ptr(stack_b);
 	return (0);
 }
