0e2f5e4f68605b0cee9e3d748eee272e77e05288
umeneses <umeneses@student.42sp.org.br>
2024-06-06 15:07:14 -0300

    verifying approved for ft_rotate and ft_rotate_ab

--- _ci_tdd/test_files/test_main_miunit.c
+++ _ci_tdd/test_files/test_main_miunit.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 19:06:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/06 15:03:02 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:06:03 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -389,88 +389,94 @@ void	ft_array_printer(char **array, int arr_size);
 // 	ft_lstclear_single_ptr(stack_a);
 // }
 
-// MU_TEST(test_ft_rotate_ab)
-// {
-// 	// ARRANGE
-// 	int		top_a = 11;
-// 	int		second_a = 22;
-// 	int		third_a = 33;
-// 	int		bottom_a = 44;
-// 	int		top_b = 999;
-// 	int		second_b = 888;
-// 	int		third_b = 777;
-// 	int		bottom_b = 666;
-// 	int		actual_result_a;
-// 	int		actual_result_b;
-// 	int		expected_result_a;
-// 	int		expected_result_b;
-// 	int		actual_size_a;
-// 	int		actual_size_b;
-// 	int		expected_size_a;
-// 	int		expected_size_b;
-// 	t_stack	*stack_a;
-// 	t_stack	*stack_b;
+MU_TEST(test_ft_rotate_ab)
+{
+	// ARRANGE
+	int		top_a = 11;
+	int		second_a = 22;
+	int		third_a = 33;
+	int		bottom_a = 44;
+	int		top_b = 999;
+	int		second_b = 888;
+	int		third_b = 777;
+	int		bottom_b = 666;
+	int		actual_result_a;
+	int		actual_result_b;
+	int		expected_result_a;
+	int		expected_result_b;
+	int		actual_size_a;
+	int		actual_size_b;
+	int		expected_size_a;
+	int		expected_size_b;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
-// 	// ACT
-// 	stack_a = NULL;
-// 	stack_b = NULL;
-// 	stack_a = ft_lst_init(top_a);
-// 	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(second_a));
-// 	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(third_a));
-// 	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(bottom_a));
-// 	stack_b = ft_lst_init(top_b);
-// 	stack_b = ft_lst_addto_end(&stack_b, ft_lst_init(second_b));
-// 	stack_b = ft_lst_addto_end(&stack_b, ft_lst_init(third_b));
-// 	stack_b = ft_lst_addto_end(&stack_b, ft_lst_init(bottom_b));
-// 	expected_result_a = second_a;
-// 	expected_result_b = second_b;
-// 	expected_size_a = ft_lstsize_int((t_list *)stack_a);
-// 	expected_size_b = ft_lstsize_int((t_list *)stack_b);
-// 	ft_rotate_ab(&stack_a, &stack_b);
-// 	actual_result_a = ft_lst_goto_head(stack_a)->nbr;
-// 	actual_result_b = ft_lst_goto_head(stack_b)->nbr;
-// 	actual_size_a = ft_lstsize_int((t_list *)stack_a);
-// 	actual_size_b = ft_lstsize_int((t_list *)stack_b);
+	// ACT
+	stack_a = NULL;
+	stack_b = NULL;
+	stack_a = ft_lst_init(top_a);
+	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(second_a));
+	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(third_a));
+	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(bottom_a));
+	stack_b = ft_lst_init(top_b);
+	stack_b = ft_lst_addto_end(&stack_b, ft_lst_init(second_b));
+	stack_b = ft_lst_addto_end(&stack_b, ft_lst_init(third_b));
+	stack_b = ft_lst_addto_end(&stack_b, ft_lst_init(bottom_b));
+	expected_result_a = second_a;
+	expected_result_b = second_b;
+	expected_size_a = ft_lstsize_int((t_list *)stack_a);
+	expected_size_b = ft_lstsize_int((t_list *)stack_b);
+	ft_lst_printf_int_content("original stack_a >>>>>>", stack_a);
+	ft_lst_printf_int_content("original stack_b >>>>>>", stack_b);
+	ft_rotate_ab(&stack_a, &stack_b);
+	actual_result_a = ft_lst_goto_head(stack_a)->nbr;
+	actual_result_b = ft_lst_goto_head(stack_b)->nbr;
+	actual_size_a = ft_lstsize_int((t_list *)stack_a);
+	actual_size_b = ft_lstsize_int((t_list *)stack_b);
+	ft_printf("stack_a size = %d\n", ft_lstsize_int((t_list *)stack_a));
+	ft_printf("stack_b size = %d\n", ft_lstsize_int((t_list *)stack_b));
+	ft_lst_printf_int_content("new stack_a >>>>>>", stack_a);
+	ft_lst_printf_int_content("new stack_b >>>>>>", stack_b);	
 
-// 	// ASSERT
-// 	mu_assert_int_eq(expected_result_a, actual_result_a);
-// 	mu_assert_int_eq(expected_result_b, actual_result_b);
-// 	mu_assert_int_eq(expected_size_a, actual_size_a);
-// 	mu_assert_int_eq(expected_size_b, actual_size_b);
-// 	ft_lstclear_single_ptr(stack_a);
-// 	ft_lstclear_single_ptr(stack_b);
-// }
+	// ASSERT
+	mu_assert_int_eq(expected_result_a, actual_result_a);
+	mu_assert_int_eq(expected_result_b, actual_result_b);
+	mu_assert_int_eq(expected_size_a, actual_size_a);
+	mu_assert_int_eq(expected_size_b, actual_size_b);
+	ft_lstclear_single_ptr(stack_a);
+	ft_lstclear_single_ptr(stack_b);
+}
 
-// MU_TEST(test_ft_rotate)
-// {
-// 	// ARRANGE
-// 	int		top_a = 11;
-// 	int		second_a = 22;
-// 	int		third_a = 33;
-// 	int		bottom_a = 44;
-// 	int		actual_result_a;
-// 	int		expected_result_a;
-// 	int		expected_size;
-// 	int		actual_size;
-// 	t_stack	*stack_a;
+MU_TEST(test_ft_rotate)
+{
+	// ARRANGE
+	int		top_a = 11;
+	int		second_a = 22;
+	int		third_a = 33;
+	int		bottom_a = 44;
+	int		actual_result_a;
+	int		expected_result_a;
+	int		expected_size;
+	int		actual_size;
+	t_stack	*stack_a;
 
-// 	// ACT
-// 	stack_a = NULL;
-// 	stack_a = ft_lst_init(top_a);
-// 	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(second_a));
-// 	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(third_a));
-// 	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(bottom_a));
-// 	expected_result_a = second_a;
-// 	expected_size = ft_lstsize_int((t_list *)stack_a);
-// 	ft_rotate(&stack_a);
-// 	actual_result_a = ft_lst_goto_head(stack_a)->nbr;
-// 	actual_size = ft_lstsize_int((t_list *)stack_a);
+	// ACT
+	stack_a = NULL;
+	stack_a = ft_lst_init(top_a);
+	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(second_a));
+	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(third_a));
+	stack_a = ft_lst_addto_end(&stack_a, ft_lst_init(bottom_a));
+	expected_result_a = second_a;
+	expected_size = ft_lstsize_int((t_list *)stack_a);
+	ft_rotate(&stack_a);
+	actual_result_a = ft_lst_goto_head(stack_a)->nbr;
+	actual_size = ft_lstsize_int((t_list *)stack_a);
 
-// 	// ASSERT
-// 	mu_assert_int_eq(expected_result_a, actual_result_a);
-// 	mu_assert_int_eq(expected_size, actual_size);
-// 	ft_lstclear_single_ptr(stack_a);
-// }
+	// ASSERT
+	mu_assert_int_eq(expected_result_a, actual_result_a);
+	mu_assert_int_eq(expected_size, actual_size);
+	ft_lstclear_single_ptr(stack_a);
+}
 
 MU_TEST(test_ft_push_b)
 {
@@ -509,17 +515,11 @@ MU_TEST(test_ft_push_b)
 	expected_result_b = top_a;
 	expected_size_a = ft_lstsize_int((t_list *)stack_a) - 1;
 	expected_size_b = ft_lstsize_int((t_list *)stack_b) + 1;
-	ft_lst_printf_int_content("original stack_a >>>>>>", stack_a);
-	ft_lst_printf_int_content("original stack_b >>>>>>", stack_b);
 	ft_push_b(&stack_a, &stack_b);
 	actual_result_a = ft_lst_goto_head(stack_a)->nbr;
 	actual_result_b = ft_lst_goto_head(stack_b)->nbr;
 	actual_size_a = ft_lstsize_int((t_list *)stack_a);
 	actual_size_b = ft_lstsize_int((t_list *)stack_b);
-	ft_printf("stack_a size = %d\n", ft_lstsize_int((t_list *)stack_a));
-	ft_printf("stack_b size = %d\n", ft_lstsize_int((t_list *)stack_b));
-	ft_lst_printf_int_content("new stack_a >>>>>>", stack_a);
-	ft_lst_printf_int_content("new stack_b >>>>>>", stack_b);
 
 	// ASSERT
 	mu_assert_int_eq(expected_result_a, actual_result_a);
@@ -956,13 +956,13 @@ MU_TEST_SUITE(push_tests)
 	MU_RUN_TEST(test_ft_push_b);
 }
 
-// MU_TEST_SUITE(rotate_tests)
-// {
-// 	MU_RUN_TEST(test_ft_rotate);
-// 	MU_RUN_TEST(test_ft_rotate_ab);
-// 	MU_RUN_TEST(test_ft_reverse_rotate);
-// 	MU_RUN_TEST(test_ft_reverse_rotate_ab);
-// }
+MU_TEST_SUITE(rotate_tests)
+{
+	MU_RUN_TEST(test_ft_rotate);
+	MU_RUN_TEST(test_ft_rotate_ab);
+	// MU_RUN_TEST(test_ft_reverse_rotate);
+	// MU_RUN_TEST(test_ft_reverse_rotate_ab);
+}
 
 // MU_TEST_SUITE(argv_tests)
 // {
@@ -986,7 +986,7 @@ int main(void)
 	MU_RUN_SUITE(linked_list_tests);
 	MU_RUN_SUITE(swap_tests);
 	MU_RUN_SUITE(push_tests);
-	// MU_RUN_SUITE(rotate_tests);
+	MU_RUN_SUITE(rotate_tests);
 	// MU_RUN_SUITE(argv_tests);
 	// MU_RUN_SUITE(sorting_tests);
 	MU_REPORT();
