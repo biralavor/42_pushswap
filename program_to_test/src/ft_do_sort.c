4645df230b8df2393effb79561b3cbb12299985e
umeneses <umeneses@student.42sp.org.br>
2024-06-07 15:09:34 -0300

    protecting unused -yet- variable

--- program_to_test/src/ft_do_sort.c
+++ program_to_test/src/ft_do_sort.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:46:55 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/06 18:53:26 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/07 15:05:22 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	ft_do_sort(t_stack **stack_a, t_stack **stack_b)
 {
-	if (!ft_is_sorted(*stack_a) || !ft_is_sorted(*stack_b))
+	(void)stack_b;
+
+	if (!ft_is_sorted(*stack_a))
 	{
 		if (ft_lstsize_int((t_list *)*stack_a) == 2)
 			ft_sort_2_nbrs(stack_a);
