f9c636b963ee2f3584b630299a3dbdb8080b5c0e
umeneses <umeneses@student.42sp.org.br>
2024-06-11 14:46:18 -0300

    fixing method

--- program_to_test/src/ft_sort_4_or_more.c
+++ program_to_test/src/ft_sort_4_or_more.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:41:29 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/11 13:26:23 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/11 13:33:42 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	ft_push_b_all_but_three(t_stack **stack_a, t_stack **stack_b)
 			pushed++;
 		}
 		else
-			ft_sort_3_nbrs(stack_a);
+			ft_do_rotate_a(stack_a);
 		index++;
 	}
 	while (stack_size - pushed > 3)
