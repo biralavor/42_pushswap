d9f9c1a31709497f8df98e17dc6bc003748b4e99
umeneses <umeneses@student.42sp.org.br>
2024-06-14 18:14:04 -0300

    adding map all indexers

---
+++ program_to_test/src/ft_lst_map_all.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lst_map_all.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 18:41:22 by umeneses          #+#    #+#             */
+/*   Updated: 2024/06/13 18:41:32 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_lst_map_all_indexers(t_stack **stack)
+{
+	int	stack_size;
+
+	stack_size = ft_lst_size(*stack);
+	ft_lst_map_position(stack);
+	ft_lst_map_weight(stack, stack_size);
+}
