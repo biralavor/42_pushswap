55d74c7e314bc96264e43e746e605e25b4b8862e
umeneses <umeneses@student.42sp.org.br>
2024-06-18 18:53:46 -0300

    applying new method -> ft_push()

--- program_to_test/src/ft_push.c
+++ program_to_test/src/ft_push.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 11:52:34 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/18 16:34:17 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/18 18:06:10 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,24 @@ void	ft_push_a(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack	*to_be_pushed;
 
-	if (*stack_a == NULL || *stack_b == NULL)
+	if (NULL == *stack_b)
 		return ;
 	to_be_pushed = (*stack_b);
 	(*stack_b) = (*stack_b)->next;
 	if ((*stack_b))
 		(*stack_b)->prev = NULL;
-	to_be_pushed->next = NULL;
-	to_be_pushed->prev = NULL;
-	to_be_pushed->next = (*stack_a);
-	(*stack_a)->prev = to_be_pushed;
+	if (NULL == (*stack_a))
+	{
+		(*stack_a) = to_be_pushed;
+		(*stack_a)->next = NULL;
+		(*stack_a)->prev = NULL;
+	}
+	else
+	{
+		to_be_pushed->next = (*stack_a);
+		to_be_pushed->next->prev = to_be_pushed;
+		(*stack_a) = to_be_pushed;
+	}
 	(*stack_a) = ft_lst_goto_head(*stack_a);
 }
 
@@ -33,15 +41,23 @@ void	ft_push_b(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack	*to_be_pushed;
 
-	if (*stack_a == NULL || *stack_b == NULL)
+	if (NULL == *stack_a)
 		return ;
 	to_be_pushed = (*stack_a);
 	(*stack_a) = (*stack_a)->next;
 	if ((*stack_a))
 		(*stack_a)->prev = NULL;
-	to_be_pushed->next = NULL;
-	to_be_pushed->prev = NULL;
-	to_be_pushed->next = (*stack_b);
-	(*stack_b)->prev = to_be_pushed;
+	if (NULL == (*stack_b))
+	{
+		(*stack_b) = to_be_pushed;
+		(*stack_b)->next = NULL;
+		(*stack_b)->prev = NULL;
+	}
+	else
+	{
+		to_be_pushed->next = (*stack_b);
+		to_be_pushed->next->prev = to_be_pushed;
+		(*stack_b) = to_be_pushed;
+	}
 	(*stack_b) = ft_lst_goto_head(*stack_b);
 }
