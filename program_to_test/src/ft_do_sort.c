ed68cc1393e895040f17cd8a0171ffa103576e9b
umeneses <umeneses@student.42sp.org.br>
2024-06-18 18:52:39 -0300

    improving readability

--- program_to_test/src/ft_do_sort.c
+++ program_to_test/src/ft_do_sort.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:46:55 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/18 14:03:19 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/18 18:12:19 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	ft_do_sort(t_stack **stack_a, t_stack **stack_b)
 {
+	int	stack_size;
+
+	stack_size = ft_lst_size(*stack_a);
 	if (!ft_is_sorted(*stack_a))
 	{
-		if (ft_lst_size(*stack_a) == 2)
+		if (stack_size == 2)
 			ft_sort_two_nbrs(stack_a);
-		else if (ft_lst_size(*stack_a) == 3)
+		else if (stack_size == 3)
 			ft_sort_three_nbrs(stack_a);
 		else
 		{
