6ae5ca49e4cdf9034f25945551b778e367b9de51
umeneses <umeneses@student.42sp.org.br>
2024-06-18 16:39:24 -0300

    updating main

--- program_to_test/src/push_main.c
+++ program_to_test/src/push_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/18 11:33:50 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/18 15:40:33 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	main(int argc, char **argv)
 	if (argc <= 2)
 		ft_error_msg("Not enough arguments. Please provide at leats 2 numbers");
 	stack_a = NULL;
-	stack_b = (t_stack *)ft_calloc(1, sizeof(t_stack));
+	stack_b = NULL;
 	stack_a = ft_lts_buildstack_argv(&stack_a, argv);
-	ft_lst_printf_data_content("original main:", stack_a);
 	ft_lst_map_all_indexers(&stack_a);
+	ft_lst_printf_data_content("original main stack_a:", stack_a);
 	ft_do_sort(&stack_a, &stack_b);
 	ft_lst_printf_data_content("updated main stack_a:", stack_a);
 	ft_lst_printf_data_content("updated main stack_b:", stack_b);
