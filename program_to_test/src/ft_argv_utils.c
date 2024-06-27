40487552115329bd028d376da857ab135a6638b7
umeneses <umeneses@student.42sp.org.br>
2024-06-27 14:34:17 -0300

    adding ft_atoi for long int

--- program_to_test/src/ft_argv_utils.c
+++ program_to_test/src/ft_argv_utils.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:35:16 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/26 15:35:26 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/27 14:33:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,31 @@ bool	ft_is_space(int c)
 	if (c == ' ')
 		return (true);
 	return (false);
+}
+
+long int	ft_atoi_long_int(const char *str)
+{
+	long int	result;
+	int			signal;
+	char		*string;
+
+	signal = 1;
+	string = (char *)str;
+	while (((*string >= 9 && *string <= 13) || *string == 32))
+		string++;
+	while ((*string == '-') || (*string == '+'))
+	{
+		if (*(string + 1) == '-' || *(string + 1) == '+')
+			return (0);
+		if (*string == '-')
+			signal = signal * -1;
+		string++;
+	}
+	result = 0;
+	while (*string >= '0' && *string <= '9')
+	{
+		result = (result * 10) + (*string - '0');
+		string++;
+	}
+	return (result * signal);
 }
