c1dc74fa721ee9ebbd3bbd89b8b10f4551c8de4a
umeneses <umeneses@student.42sp.org.br>
2024-07-04 19:01:09 -0300

    clearing main push

--- program_to_test/src/main.c
+++ program_to_test/src/main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/01 15:32:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/04 19:00:14 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,7 @@ int	main(int argc, char **argv)
 	stack_b = NULL;
 	stack_a = ft_lts_buildstack_argv(&stack_a, argv);
 	ft_lst_map_all_indexers(&stack_a);
-	ft_lst_printf_data_content("origin stack_a:", stack_a);
 	ft_do_sort(&stack_a, &stack_b);
-	ft_lst_printf_data_content("// final stack_a:", stack_a);
-	if (stack_b)
-		ft_lst_printf_data_content("// final stack_b:", stack_b);
 	ft_lstclear_single_ptr(stack_a);
 	return (0);
 }
