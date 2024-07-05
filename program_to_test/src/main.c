eb35fd0f972b987b824c138f33a701fda5d917a9
umeneses <umeneses@student.42sp.org.br>
2024-07-05 13:37:37 -0300

    enabling stack printer

--- program_to_test/src/main.c
+++ program_to_test/src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_main.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 18:34:31 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/04 19:00:14 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/05 13:09:05 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,10 @@ int	main(int argc, char **argv)
 	stack_b = NULL;
 	stack_a = ft_lts_buildstack_argv(&stack_a, argv);
 	ft_lst_map_all_indexers(&stack_a);
+	ft_lst_printf_data_content("original list", stack_a);
 	ft_do_sort(&stack_a, &stack_b);
+	ft_lst_printf_data_content("stack_a", stack_a);
+	ft_lst_printf_data_content("stack_b", stack_b);
 	ft_lstclear_single_ptr(stack_a);
 	return (0);
 }
